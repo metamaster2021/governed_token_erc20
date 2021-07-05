5c3ee65a199a3e02d6741b0f8f824c7df2c4d3cb
tigerich <tigerich@163.com>
2021-07-06 00:37:45 +0800

    fix safemath usage

--- HiToken.sol
+++ HiToken.sol
@@ -407,6 +407,8 @@ contract MintableToken is StandardToken, Ownable {
  */
 contract HiToken is PausableToken, FrozenableToken, MintableToken
 {
+    using SafeMath for uint256;
+
     string public name = "hi Dollars";
     string public symbol = "HI";
     uint256 public decimals = 18;
@@ -535,7 +537,7 @@ contract HiToken is PausableToken, FrozenableToken, MintableToken
       uint256 unsplitted = _amount;
       for (uint8 i = 0; i < _totalHolders - 1; i++) {
         address _to = holders[i];
-        uint256 _amt = div(mul(_amount, MintSplitHolderRatios), 10000);
+        uint256 _amt = _amount.mul(MintSplitHolderRatios).div(10000);
         unsplitted -= _amt;
         _mint(_to, _amt);
       }
