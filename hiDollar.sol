d7e4f9c42249d84915f8a6b55b8cb33c26fda041
tigerich <tigerich@163.com>
2021-07-31 19:33:09 +0800

    change holder addresses

--- hiDollar.sol
+++ hiDollar.sol
@@ -394,12 +394,12 @@ contract hiDollar is PausableToken, FrozenableToken, MintableToken
      * @dev Initializes the total release
      */
     constructor() public {
-        holders[0] = 0xb660539dd01A78ACB3c7CF77BfcCE735081ec004; //HI_LID
-        holders[1] = 0x8376EEF57D86A8c1DFEE8E91E75912e361A940e0; //HI_EG
-        holders[2] = 0x572aB5eC71354Eb80e6D18e394b3e71BA8e282F5; //HI_NLTI
-        holders[3] = 0x93aeC0ADc392C09666B4d56654F39a375AEbD4C1; //HI_CR
-        holders[4] = 0xFb3BEb5B1258e438982956c9f023d4F7bD683E4E; //HI_FR
-        holders[5] = 0xBF990D24F7167b97b836457d380ACCdCb1782201; //HI_FT
+        holders[0] = 0x3AeEFC8dDf3f0787c216b60ea732163455B5163b; //HI_LID
+        holders[1] = 0x142Fc12540Dc0FA4e24fB7BedB220397A9e77bF5; //HI_EG
+        holders[2] = 0xb1E5f7E027C4ecd9b52C0C285bb628351CF0F5c6; //HI_NLTI
+        holders[3] = 0x300Ae38eC675756Ad53bf8c579A0aEC9E09de1D7; //HI_CR
+        holders[4] = 0x592f9A9c2F16D27Db43c0Df28e53D0D6184db1C4; //HI_FR
+        holders[5] = 0xFa318277eC9633709d5cE641DD8ca80BB9Cc733D; //HI_FT
 
         MintSplitHolderRatios[0] = 2720; //27.2%
         MintSplitHolderRatios[1] = 1820; //18.2%
